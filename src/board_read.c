4803aa99c2681e190f34db6ef38eea011098ecf1
rus <rus@russ-MacBook-Pro.local>
2018-04-23 02:14:47 +0700

    Split board_read on 2 functions

--- src/board_read.c
+++ src/board_read.c
@@ -2,16 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *getInput() {
+char *board_read() {
     char *str = calloc(7, sizeof(char));
     do {
     scanf("%6s", str);
-    } while(str[0] < 'a' || str[0] > 'h'
+    } while (check_input(str));
+    return str;
+}
+
+int check_input(char *str) {
+    if (str[0] < 'a' || str[0] > 'h'
         || str[1] < '0' || str[1] > '8'
         || str[2] != '-'
         || str[3] < 'a' || str[3] > 'h'
         || str[4] < '0' || str[4] > '9'
-        || str[5] != '\0'
-      );
-    return str;
+        || str[5] != '\0') {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
