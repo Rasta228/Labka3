90392bbbb791db6eae7606d761bf5f73693e140e
rus <rus@russ-MacBook-Pro.local>
2018-04-23 11:24:40 +0700

    Add incorrect test

--- test/main.c
+++ test/main.c
@@ -1,6 +1,8 @@
 #define CTEST_MAIN
 
 #include <board_read.h>
+#include <board_start.h>
+#include <board.h>
 
 #include <ctest.h>
 
@@ -13,6 +15,28 @@ CTEST(Syntax, Correct) {
 CTEST(Syntax, Incorrect) {
     int result = check_input("a2-a9");
     int expected = 1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Black_pwn, Correct) {
+    char deck[8][8];
+    board_start(deck);
+    char input[] = "a7-a5";
+    int move[4];
+    reformat_input(input, move);
+    int result = black_pwn(deck, move);
+    int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(Black_pwn, Incorrect) {
+    char deck[8][8];
+    board_start(deck);
+    char input[] = "a7-b6";
+    int move[4];
+    reformat_input(input, move);
+    int result = black_pwn(deck, move);
+    int expected = 0;
     ASSERT_EQUAL(expected, result);
 }
 
